15e46ed780357e9ff31409c037cc25ea04135fb3
Gian Moran <gpmoran_@outlook.com>
2020-01-29 22:04:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+require 'pry'
+class Dog 
+  @@all = []
+  attr_accessor :name
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end
+
+  def self.all
+    @@all 
+  end
+  def self.clear_all
+   @@all.clear
+  end
+  def self.print_all
+   @@all.map { |x| puts x.name}
+  end
+  def save
+   @@all.map { |x| puts x.name}
+    binding.pry
+  end
+
+end
